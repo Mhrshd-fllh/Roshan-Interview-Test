264c5d3fda0d1dce9f4c4a110e69418330a87898
Mehrshad Fallah Estalkhzir <fallah_mehrshad82@comp.iust.ac.ir>
2025-12-31 22:18:22 +0330

    feat: add admin configuration for documents and qa

--- apps/documents/admin.py
+++ apps/documents/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Document, Tag
 
-# Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['id', 'name']
+    ordering = ['name']
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['id', 'title', 'created_at']
+    list_filter = ['created_at', 'tags']
+    autocomplete_fields = ['tags']
+    ordering = ['-created_at']
+
+    
+
