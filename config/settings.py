9d7177d521232c3b0ea1b9e74f734209bf86be77
Mehrshad Fallah <115925170+Mhrshd-fllh@users.noreply.github.com>
2026-01-01 22:06:18 +0330

    feat: add swagger schema and ui endpoints (#5)

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,15 @@ INSTALLED_APPS = [
     'apps.qa.apps.QaConfig',
 ]
 
+INSTALLED_APPS += [
+    "rest_framework",
+    "drf_spectacular",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from apps.qa.api import retrieve_view
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/retrieve/", retrieve_view)
+    path("api/retrieve/", retrieve_view),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
