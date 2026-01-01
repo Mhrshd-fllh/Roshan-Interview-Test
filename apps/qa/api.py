ed3b881f740c2f7f7d25fd893e5d1270996e9527
Mehrshad Fallah Estalkhzir <fallah_mehrshad82@comp.iust.ac.ir>
2026-01-01 21:07:16 +0330

    fix: fixing tfidf retrieval api endpoint

--- apps/qa/api.py
+++ apps/qa/api.py
@@ -2,11 +2,9 @@ import json
 
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt
 
 from apps.documents.services.retrieval import retrieve_top_k
 
-@csrf_exempt
 @require_POST
 def retrieve_view(request):
     payload = json.loads(request.body.decode("utf-8") or "{}")
